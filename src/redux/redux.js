ed157e2834fab098cbeb4931c081efbe335cfc72
Veronica De Leon <v.dleon@gmail.com>
2020-11-19 16:54:23 +0100

    async actions

--- src/redux/redux.js
+++ src/redux/redux.js
@@ -1,9 +1,9 @@
 import { applyMiddleware, createStore } from "redux";
 import { combineReducers } from "redux"
 import logger from 'redux-logger'
-import { createLogger } from 'redux-logger'
-//import { composeWithDevTools } from 'redux-devtools-extension'
-//import thunk from 'redux-thunk'
+// import { createLogger } from 'redux-logger'
+// import { composeWithDevTools } from 'redux-devtools-extension'
+// import thunk from 'redux-thunk'
  
 // ACTIONS
 
